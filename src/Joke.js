7469ba369d6c259d695012715840327006dcba53
RQuicho <raymondquicho@gmail.com>
2023-10-31 22:32:10 -0700

    refactoring code to use hooks

--- src/Joke.js
+++ src/Joke.js
@@ -1,30 +1,34 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./Joke.css";
 
 /** A single joke, along with vote up/down buttons. */
 
-class Joke extends Component {
-  render() {
-    const { id, vote, votes, text } = this.props;
-
-    return (
-      <div className="Joke">
-        <div className="Joke-votearea">
-          <button onClick={evt => vote(id, +1)}>
-            <i className="fas fa-thumbs-up" />
-          </button>
-
-          <button onClick={evt => vote(id, -1)}>
-            <i className="fas fa-thumbs-down" />
-          </button>
-
-          {votes}
-        </div>
-
-        <div className="Joke-text">{text}</div>
-      </div>
-    );
+const Joke = ({id, vote, votes, text}) => {
+  const handleClickUp = () => {
+    vote(id, +1);
   }
+  const handleClickDown = () => {
+    vote(id, -1);
+  }
+
+
+  return (
+    <div className="Joke">
+    <div className="Joke-votearea">
+      <button onClick={handleClickUp}>
+        <i className="fas fa-thumbs-up" />
+      </button>
+
+      <button onClick={handleClickDown}>
+        <i className="fas fa-thumbs-down" />
+      </button>
+
+      {votes}
+    </div>
+
+    <div className="Joke-text">{text}</div>
+  </div>
+  )
 }
 
 export default Joke;
